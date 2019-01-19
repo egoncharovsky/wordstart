3244807fd02d6d81c377d0a57add892e14776835
Evgeny Goncharovsky <egoncharovsky@gmail.com>
2019-01-20 01:53:28 +0300

    simple navigation&top bar

--- app/src/main/kotlin/ru/egoncharovsky/wordstart/ui/MainActivity.kt
+++ app/src/main/kotlin/ru/egoncharovsky/wordstart/ui/MainActivity.kt
@@ -1,15 +1,18 @@
 package ru.egoncharovsky.wordstart.ui
 
-import android.content.res.ColorStateList
 import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
 import android.view.View
+import android.widget.LinearLayout
 import org.jetbrains.anko.*
-import org.jetbrains.anko.design.longSnackbar
-import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.jetbrains.anko.appcompat.v7.toolbar
+import org.jetbrains.anko.design.coordinatorLayout
+import org.jetbrains.anko.design.navigationView
+import org.jetbrains.anko.design.themedAppBarLayout
+import org.jetbrains.anko.support.v4.drawerLayout
 import ru.egoncharovsky.wordstart.R
 
 class MainActivity : AppCompatActivity() {
@@ -20,43 +23,67 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+
 class MainActivityContent : AnkoComponent<MainActivity> {
     override fun createView(ui: AnkoContext<MainActivity>): View = with(ui) {
-        frameLayout {
-            val textField = editText {
-                hint = "Text for toasts and snackbars"
-            }.lparams(width = matchParent) {
-                margin = dip(12)
-                topMargin = dip(30)
-            }
+        drawerLayout {
+            fitsSystemWindows = true
 
-            imageView(R.drawable.ic_android_black_24dp) {
-                onClick {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-                        this@imageView.imageTintList =
-                                ColorStateList.valueOf(Color.parseColor("#4CAF50"))
-                }
-            }.lparams (dip(72), dip(72)) {
-                gravity = Gravity.CENTER
-            }
+            coordinatorLayout {
+                //                fitsSystemWindows = true
 
-            linearLayout {
-                button("Show toast") {
-                    onClick {
-                        toast(textField.text)
+                themedAppBarLayout(R.style.AppTheme_AppBarOverlay) {
+                    //
+                    toolbar {
+                        lparams(width = matchParent, height = matchParent)
+                        popupTheme = R.style.AppTheme_PopupOverlay
+
+                        textView("toolbar")
                     }
+                }.lparams(width = matchParent, height = wrapContent)
+
+                relativeLayout {
+                    horizontalPadding = dimen(R.dimen.activity_horizontal_margin)
+                    verticalPadding = dimen(R.dimen.activity_vertical_margin)
+
+                    textView("Hello World!")
+                }.lparams(width = matchParent, height = matchParent) {
+                    behavior = AppBarLayout.ScrollingViewBehavior()
+                }
+            }.lparams(width = matchParent, height = matchParent)
+
+            navigationView {
+                fitsSystemWindows = true
+
+                verticalLayout {
+                    lparams(R.style.ThemeOverlay_AppCompat_Dark)
+                    backgroundResource = R.drawable.side_nav_bar
+                    gravity = Gravity.BOTTOM
+                    orientation = LinearLayout.VERTICAL
+                    padding = dip(20)
+
+                    imageView(R.mipmap.ic_launcher_round) {
+                        padding = dip(5)
+                    }.lparams(width = wrapContent, height = wrapContent)
+
+                    textView(R.string.nav_header_title) {
+                        padding = R.dimen.nav_header_vertical_spacing
+                    }.lparams(width = matchParent, height = wrapContent)
+
+                    textView(R.string.nav_header_subtitle) {
+                        textColor = Color.WHITE
+                    }.lparams(width = wrapContent, height = wrapContent)
                 }
 
-                button("Show snackbar") {
-                    onClick {
-                        longSnackbar(this@frameLayout, textField.text.toString())
-                    }
-                }
-            }.lparams {
-                gravity = Gravity.BOTTOM or Gravity.CENTER_HORIZONTAL
-                bottomMargin = dip(72)
+            }.lparams(height = matchParent) {
+                gravity = Gravity.START
             }
         }
     }
-
 }
+
+//
+//
+//
+//
+//
